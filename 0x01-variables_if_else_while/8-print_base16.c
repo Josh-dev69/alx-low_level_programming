5eadae36da3446444eaa4817b4a7a5aa2a414e36
Josh-dev69 <ebubejoshua69@gmail.com>
2022-12-09 10:39:09 -0800

    Print Base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry
+ * Return: Always 0 (SUCCESS)
+ */
+int main(void)
+{
+	int num;
+	char alpha;
+
+	for (num = '0'; num <= '9'; num++)
+		putchar(num);
+	for (alpha = 'a'; alpha <= 'e'; alpha++)
+		putchar(alpha);
+	putchar('\n');
+	return (0);
+}
