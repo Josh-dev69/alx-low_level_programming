2bcb30a4ad6ec65fa741ce798719eec7cc1e81ed
Josh-dev69 <ebubejoshua69@gmail.com>
2022-12-09 10:30:07 -0800

    Backward Alphabet

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alpha;
+
+	for (alpha = 'z'; alpha >= 'a'; --alpha)
+	{
+		putchar(alpha);
+	}
+	putchar('\n');
+	return (0);
+}
