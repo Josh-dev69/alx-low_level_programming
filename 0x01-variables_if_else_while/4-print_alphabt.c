19e1bf2736b9273d7570775dfe602c805a792faa
Josh-dev69 <ebubejoshua69@gmail.com>
2022-12-09 02:45:57 -0800

    Print all alphabet but omit e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alpha, e, q;
+
+	e = 'e';
+	q = 'q';
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		if (alpha != e && alpha != q)
+			putchar(alpha);
+	}
+	putchar('\n');
+
+	return (0);
+}
