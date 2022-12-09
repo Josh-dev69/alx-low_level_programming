d7dcdf4c52580105eaf7fbdc0c6957737b48f0de
Josh-dev69 <ebubejoshua69@gmail.com>
2022-12-09 14:46:03 -0800

    Combination of digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-	int num1.num2;
+	int num1, num2;
 
 	for (num1 = '0'; num1 < '9'; num1++)
 	{
@@ -25,4 +25,7 @@ int main(void)
 			}
 		}
 	}
+	putchar('\n');
+
+	return (0);
 }
