40a96023ef04596a3b1537a2d6197e6aae29f99d
Josh-dev69 <ebubejoshua69@gmail.com>
2022-12-13 09:35:33 -0800

    Print range from n to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - Print numbers from input n to ninety eight
+ * @n: Input character
+ * Return: Nothing
+ */
+
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		for (; n <= 98; n++)
+		{
+			printf("%d", n);
+
+			if (n == 98)
+				continue;
+			printf(", ");
+		}
+		printf("\n");
+	}
+	else
+	{
+		for (; n >= 98; n--)
+		{
+			printf("%d", n);
+
+			if (n == 98)
+				continue;
+			printf(", ");
+		}
+		printf("\n");
+	}
+}
