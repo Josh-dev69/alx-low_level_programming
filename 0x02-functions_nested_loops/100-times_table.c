1937ca0c4624e495ecc364b58e92ebf9dcf43243
Josh-dev69 <ebubejoshua69@gmail.com>
2022-12-13 09:56:16 -0800

    Print multiplication table from 0 to n range

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ *  print_times_table - Print the times table to the range of the input character
+ *  @n: input character
+ *  Return: Nothing
+ */
+
+void print_times_table(int n)
+{
+	int i, j, res;
+
+	for (i = 0; i <= n; i++)
+	{
+		for (j = 0; j <= n; j++)
+		{
+			res = i * j;
+
+			if ((res / 10) == 0)
+			{
+				if (j != 0)
+					_putchar(' ');
+				_putchar(res + '0');
+
+				if (j == n)
+					continue;
+				_putchar(',');
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar((res / 10) + '0');
+				_putchar((res % 10) + '0');
+
+				if (j == n)
+					continue;
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
