ac4b37fbd7ddb31430d1fddecb8640c0a0faced8
Josh-dev69 <ebubejoshua69@gmail.com>
2022-12-14 08:53:55 -0800

    Multiplication table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -15,7 +15,8 @@ void print_times_table(int n)
 		for (i = 0; i <= n; i++)
 		{
 			_putchar('0');
-			for (j = 0; j <= n; j++)
+
+			for (j = 1; j <= n; j++)
 			{
 				_putchar(',');
 				_putchar(' ');
@@ -28,8 +29,8 @@ void print_times_table(int n)
 					_putchar(' ');
 				if (res >= 100)
 				{
-					_putchar('0' + (res / 100));
-					_putchar('0' + ((res / 10) % 10));
+					_putchar((res / 100) + '0');
+					_putchar((res / 10) % 10 + '0');
 				}
 				else if (res <= 99 && res >= 10)
 				{
