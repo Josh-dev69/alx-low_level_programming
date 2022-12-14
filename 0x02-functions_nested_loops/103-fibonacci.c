faabd1d6fd5d1734d4e46e7b17e6fb7d7dfa9578
Josh-dev69 <ebubejoshua69@gmail.com>
2022-12-14 05:28:53 -0800

    Sum of even numbers in a Fibonacci

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -7,4 +7,21 @@
 
 int main(void)
 {
-	int
+	int i, sum;
+	long int fib[4000000];
+
+	fib[0] = 1;
+	fib[1] = 2;
+	sum = 0;
+
+	for (i = 2; i < 4000000; i++)
+	{
+		fib[i] = fib[i - 1] + fib[i - 2];
+		if (fib[i] % 2 == 0)
+		{
+			sum = sum + fib[i];
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
