6c63a9916b21688c8fc2644d0322b640d1f1ca03
Josh-dev69 <ebubejoshua69@gmail.com>
2022-12-19 04:57:31 -0800

    C - pointers, array and string (Mandatory task completed)

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,13 +9,11 @@
 void print_rev(char *s)
 {
 	int i, len = 0;
-	
 	/* Determine the length of the string */
 	while (s[len] != '\0')
 	{
 		len++;
 	}
-	
 	/* Loop to print each character in the string in a reverse order */
 	for (i = len - 1; i >= 0; i--)
 	{
