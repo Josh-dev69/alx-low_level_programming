a401facd4fa7e65ebd57025415be032bc3d4b9e0
Josh-dev69 <ebubejoshua69@gmail.com>
2022-12-19 05:16:13 -0800

    C - pointers, array and string (Mandatory task completed)

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -11,22 +11,14 @@ void puts_half(char *str)
 	int i, n, len = 0;
 	/* Loop to check the length of the string */
 	while (str[len] != '\0')
-	{
 		len++;
-	}
 	/* condition that determine the start of the printing */
 	if (len % 2 == 0)
-	{
 		n = len / 2;
-	}
 	else
-	{
 		n = (len - 1) / 2;
-	}
 	/* Loop for Printing the half */
 	for (i = n; i < len; i++)
-	{
 		_putchar(str[i]);
-	}
 	_putchar('\n');
 }
