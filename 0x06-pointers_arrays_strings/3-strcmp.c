ef60f9acfe47f058454bb85ee28e7d54a4ec09a4
Josh-dev69 <ebubejoshua69@gmail.com>
2022-12-21 13:34:33 -0800

    Task 4 done

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,14 +8,14 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i, diff = 0;
+	int i;
 
 	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
 	{
 		if (s1[i] != s2[i])
 		{
-			diff = s1[i] - s2[i];
+		       return (s1[i] - s2[i]);
 		}
 	}
-	return (diff);
+	return (0);
 }
