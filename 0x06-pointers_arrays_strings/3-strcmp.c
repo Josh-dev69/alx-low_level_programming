41b099e2f0b458cb8f99758894a99436e3735937
Josh-dev69 <ebubejoshua69@gmail.com>
2022-12-21 13:41:58 -0800

    Task 4 completed

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,17 +4,17 @@
  * _strcmp- Function that compares two strings
  * @s1: First String
  * @s2: Second String
- * Return: An integer value
+ * Return: 0 if equal, otherwise signed integers
  */
 int _strcmp(char *s1, char *s2)
 {
 	int i;
-
+	
 	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
 	{
 		if (s1[i] != s2[i])
 		{
-		       return (s1[i] - s2[i]);
+			return (s1[i] - s2[i]);
 		}
 	}
 	return (0);
