dd9310bf9a36e8df9885c9b892be50cca4441b0b
Josh-dev69 <ebubejoshua69@gmail.com>
2023-01-16 09:11:29 -0800

    Project completed

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,12 +1,20 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * dog - tag name the structure
+ * @name: name member of the structure
+ * @age: age member of the structure
+ * @owner: Owner member of the structure
+ */
+
 struct dog
 {
 	char* name;
 	float age;
 	char* owner;
 };
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 typedef struct dog dog_t;
